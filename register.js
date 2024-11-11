71daec15a5093a404657ba6dbe2283cda33da630
Csodo <tihamer.bodo@yahoo.com>
2024-11-11 17:50:44 +0200

    Hower password reveal, and register password revealing

--- register.js
+++ register.js
@@ -40,3 +40,45 @@ document.querySelector('#register-form').addEventListener('submit', function (ev
     });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    // Reference to the eye icon and password input field
+    const eyeIcon = document.getElementById('eye-icon');
+    const eyeIcon_2 = document.getElementById('eye-icon_2');
+    const passwordInput = document.querySelector('input[name="password"]');
+    const conf_passwordInput = document.querySelector('input[name="confirm_password"]');
+
+    // Event listener to toggle password visibility when the eye icon is clicked
+    eyeIcon.addEventListener('mousedown', function () {
+        // Reveal the password when cursor is over the icon
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text'; // Show the password
+            eyeIcon.innerHTML = '&#128064'; // Change to closed eye (with slash)
+        }
+    });
+
+    eyeIcon.addEventListener('mouseup', function () {
+        // Hide the password when cursor leaves the icon
+        if (passwordInput.type === 'text') {
+            passwordInput.type = 'password'; // Hide the password
+            eyeIcon.innerHTML = '&#128065;'; // Change to open eye
+        }
+    });
+
+
+    eyeIcon_2.addEventListener('mousedown', function () {
+        // Reveal the password when cursor is over the icon
+        if (conf_passwordInput.type === 'password') {
+            conf_passwordInput.type = 'text'; // Show the password
+            eyeIcon_2.innerHTML = '&#128064'; // Change to closed eye (with slash)
+        }
+    });
+
+    eyeIcon_2.addEventListener('mouseup', function () {
+        // Hide the password when cursor leaves the icon
+        if (conf_passwordInput.type === 'text') {
+            conf_passwordInput.type = 'password'; // Hide the password
+            eyeIcon_2.innerHTML = '&#128065;'; // Change to open eye
+        }
+    });
+});
+
